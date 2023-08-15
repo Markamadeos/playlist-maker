26a74a0854e2b34c3b744c2190f97a08af5b1581
magway <julipaha@gmail.com>
2023-08-15 17:29:29 +0300

    2. refactoring search view

--- app/src/main/java/com/guap/vkr/playlistmaker/screens/SearchActivity.kt
+++ app/src/main/java/com/guap/vkr/playlistmaker/screens/SearchActivity.kt
@@ -10,7 +10,6 @@ import android.os.PersistableBundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
-import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import com.google.gson.Gson
@@ -23,28 +22,25 @@ import com.guap.vkr.playlistmaker.databinding.ActivitySearchBinding
 import com.guap.vkr.playlistmaker.model.Track
 import com.guap.vkr.playlistmaker.utils.SHARED_PREFERENCES
 import com.guap.vkr.playlistmaker.utils.TRACK
-import com.guap.vkr.playlistmaker.utils.iTunesBaseUrl
+import com.guap.vkr.playlistmaker.utils.retrofit
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 class SearchActivity : AppCompatActivity() {
 
-    private val retrofit =
-        Retrofit.Builder().baseUrl(iTunesBaseUrl)
-            .addConverterFactory(GsonConverterFactory.create()).build()
+
     private val iTunesService = retrofit.create(ITunesApi::class.java)
     private val tracks = ArrayList<Track>()
     private val searchAdapter = TrackAdapter(tracks) { trackClickListener(it) }
     private val searchRunnable = Runnable { search() }
     private val handler = Handler(Looper.getMainLooper())
     private var userInput = ""
-    private var isClickAllowed = true
+    private var clickAllowed = true
     private lateinit var searchHistory: SearchHistory
     private lateinit var historyAdapter: TrackAdapter
     private lateinit var binding: ActivitySearchBinding
+    private var screenState = RequestState.DEFAULT_STATE
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -68,9 +64,11 @@ class SearchActivity : AppCompatActivity() {
         override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
         override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-            binding.btnClear.visibility = clearButtonVisibility(s)
-            binding.placeholderSearchHistory.visibility =
-                searchHistoryVisibility(s, searchHistory, binding.etSearch.hasFocus())
+            binding.apply {
+                btnClear.visibility = clearButtonVisibility(s)
+                placeholderSearchHistory.visibility =
+                    searchHistoryVisibility(s, searchHistory, binding.etSearch.hasFocus())
+            }
             userInput = s.toString()
             searchDebounce()
         }
@@ -105,17 +103,13 @@ class SearchActivity : AppCompatActivity() {
     }
 
     private fun queryInputConfig(textWatcher: TextWatcher) {
-        binding.etSearch.addTextChangedListener(textWatcher)
-        binding.etSearch.setText(userInput)
-        binding.etSearch.setOnEditorActionListener { _, actionId, _ ->
-            if (actionId == EditorInfo.IME_ACTION_DONE) {
-                search()
+        binding.etSearch.apply {
+            addTextChangedListener(textWatcher)
+            setText(userInput)
+            setOnFocusChangeListener { _, hasFocus ->
+                binding.placeholderSearchHistory.visibility =
+                    searchHistoryVisibility(text, searchHistory, hasFocus)
             }
-            false
-        }
-        binding.etSearch.setOnFocusChangeListener { _, hasFocus ->
-            binding.placeholderSearchHistory.visibility =
-                searchHistoryVisibility(binding.etSearch.text, searchHistory, hasFocus)
         }
     }
 
@@ -151,57 +145,40 @@ class SearchActivity : AppCompatActivity() {
     }
 
     private fun searchDebounce() {
-        handler.removeCallbacks(searchRunnable)
-        handler.postDelayed(searchRunnable, SEARCH_DEBOUNCE_DELAY_MS)
+        handler.apply {
+            removeCallbacks(searchRunnable)
+            postDelayed(searchRunnable, SEARCH_DEBOUNCE_DELAY_MS)
+        }
     }
 
     private fun search() {
         if (userInput.isNotEmpty()) {
-            binding.errorContainer.visibility = View.GONE
-            binding.placeholderSearchHistory.visibility = View.GONE
-            binding.rvSearchResult.visibility = View.GONE
-            binding.progressBar.visibility = View.VISIBLE
+            screenState = RequestState.LOADING
+            updateScreen()
             iTunesService.search(userInput).enqueue(object : Callback<SearchResponse> {
                 override fun onResponse(
                     call: Call<SearchResponse>, response: Response<SearchResponse>
                 ) {
-                    binding.progressBar.visibility = View.GONE
-                    if (response.code() == 200) {
+                    tracks.clear()
+                    screenState = if (response.code() == RESPONSE_OK) {
                         if (response.body()?.results?.isNotEmpty() == true) {
-                            tracks.clear()
                             tracks.addAll(response.body()?.results!!)
-                            binding.rvSearchResult.visibility = View.VISIBLE
-                            binding.errorContainer.visibility = View.GONE
-                            searchAdapter.notifyDataSetChanged()
+                            RequestState.GOOD_RESPONSE
                         } else {
-                            showErrorMessage(EMPTY_RESPONSE)
+                            RequestState.EMPTY_RESPONSE
                         }
 
                     } else {
-                        showErrorMessage(NETWORK_ERROR)
+                        RequestState.NETWORK_ERROR
                     }
+                    updateScreen()
                 }
 
                 override fun onFailure(call: Call<SearchResponse>, t: Throwable) {
-                    binding.progressBar.visibility = View.GONE
-                    showErrorMessage(NETWORK_ERROR)
+                    screenState = RequestState.NETWORK_ERROR
+                    updateScreen()
                 }
             })
-        }
-    }
-
-    private fun showErrorMessage(status: String) {
-        tracks.clear()
-        searchAdapter.notifyDataSetChanged()
-        binding.errorContainer.visibility = View.VISIBLE
-        if (status == EMPTY_RESPONSE) {
-            binding.ivErrorMessage.setImageResource(R.drawable.ic_search_err)
-            binding.tvErrorMessage.setText(R.string.error_nothing_found)
-            binding.btnRefresh.visibility = View.GONE
-        } else {
-            binding.ivErrorMessage.setImageResource(R.drawable.ic_internet_err)
-            binding.tvErrorMessage.setText(R.string.error_network_faild)
-            binding.btnRefresh.visibility = View.VISIBLE
         }
     }
 
@@ -216,29 +193,73 @@ class SearchActivity : AppCompatActivity() {
     }
 
     private fun trackClickListener(track: Track) {
-        if (clickDebounce()) {
+        if (isClickAllowed()) {
             searchHistory.addTrackToHistory(track)
-            historyAdapter.notifyDataSetChanged()
             val playIntent =
                 Intent(this, PlayerActivity::class.java).putExtra(TRACK, Gson().toJson(track))
             startActivity(playIntent)
         }
     }
 
-    private fun clickDebounce(): Boolean {
-        val current = isClickAllowed
-        if (isClickAllowed) {
-            isClickAllowed = false
-            handler.postDelayed({ isClickAllowed = true }, CLICK_DEBOUNCE_DELAY_MS)
+    private fun isClickAllowed(): Boolean {
+        val current = clickAllowed
+        if (clickAllowed) {
+            clickAllowed = false
+            handler.postDelayed({ clickAllowed = true }, CLICK_DEBOUNCE_DELAY_MS)
         }
         return current
     }
 
+    private fun updateScreen() {
+        binding.apply {
+            when (screenState) {
+                RequestState.GOOD_RESPONSE -> {
+                    progressBar.visibility = View.GONE
+                    rvSearchResult.visibility = View.VISIBLE
+                    errorContainer.visibility = View.GONE
+                }
+
+                RequestState.NETWORK_ERROR -> {
+                    progressBar.visibility = View.GONE
+                    errorContainer.visibility = View.VISIBLE
+                    ivErrorMessage.setImageResource(R.drawable.ic_internet_err)
+                    tvErrorMessage.setText(R.string.error_network_faild)
+                    btnRefresh.visibility = View.VISIBLE
+                }
+
+                RequestState.EMPTY_RESPONSE -> {
+                    progressBar.visibility = View.GONE
+                    errorContainer.visibility = View.VISIBLE
+                    ivErrorMessage.setImageResource(R.drawable.ic_search_err)
+                    tvErrorMessage.setText(R.string.error_nothing_found)
+                    btnRefresh.visibility = View.GONE
+                }
+
+                RequestState.LOADING -> {
+                    errorContainer.visibility = View.GONE
+                    placeholderSearchHistory.visibility = View.GONE
+                    rvSearchResult.visibility = View.GONE
+                    progressBar.visibility = View.VISIBLE
+                }
+
+                RequestState.DEFAULT_STATE -> {}
+            }
+        }
+        searchAdapter.notifyDataSetChanged()
+    }
+
+    enum class RequestState {
+        DEFAULT_STATE,
+        GOOD_RESPONSE,
+        NETWORK_ERROR,
+        EMPTY_RESPONSE,
+        LOADING
+    }
+
     companion object {
         private const val SEARCH_DEBOUNCE_DELAY_MS = 2000L
-        private const val CLICK_DEBOUNCE_DELAY_MS = 1000L
+        private const val CLICK_DEBOUNCE_DELAY_MS = 500L
         private const val USER_INPUT = "USER_INPUT"
-        private const val NETWORK_ERROR = "NETWORK_ERROR"
-        private const val EMPTY_RESPONSE = "EMPTY_RESPONSE"
+        private const val RESPONSE_OK = 200
     }
 }
