43fe916526e4312eba1a6d109dc039b6fc63a466
Mark Quarasique <julipaha@gmail.com>
2023-09-24 14:36:32 +0300

    intent error handler added

--- app/src/main/java/com/guap/vkr/playlistmaker/sharing/data/ExternalNavigatorImpl.kt
+++ app/src/main/java/com/guap/vkr/playlistmaker/sharing/data/ExternalNavigatorImpl.kt
@@ -3,9 +3,11 @@ package com.guap.vkr.playlistmaker.sharing.data
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.widget.Toast
 import com.guap.vkr.playlistmaker.R
 import com.guap.vkr.playlistmaker.sharing.domain.ExternalNavigator
 import com.guap.vkr.playlistmaker.sharing.domain.model.EmailData
+import java.lang.Exception
 
 class ExternalNavigatorImpl(private val context: Context) : ExternalNavigator {
 
@@ -33,16 +35,18 @@ class ExternalNavigatorImpl(private val context: Context) : ExternalNavigator {
             action = Intent.ACTION_SENDTO
             data = Uri.parse("mailto:")
             putExtra(Intent.EXTRA_EMAIL, arrayOf(context.getString(R.string.developer_email)))
-            putExtra(
-                Intent.EXTRA_SUBJECT,
-                context.getString(R.string.letter_topic)
-            )
-            putExtra(
-                Intent.EXTRA_TEXT,
-                context.getString(R.string.letter_body)
-            )
+            putExtra(Intent.EXTRA_SUBJECT, context.getString(R.string.letter_topic))
+            putExtra(Intent.EXTRA_TEXT, context.getString(R.string.letter_body))
+        }
+        try {
+            context.startActivity(feedbackIntent)
+        } catch (e: Exception) {
+            Toast.makeText(
+                    context,
+                    context.getString(R.string.mail_app_not_found),
+                    Toast.LENGTH_LONG
+            ).show()
         }
 
-        context.startActivity(feedbackIntent)
     }
 }
