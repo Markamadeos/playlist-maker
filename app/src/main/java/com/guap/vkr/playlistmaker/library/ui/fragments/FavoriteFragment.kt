2b342c6d19ecfa31dc83322fa0e4f1de89f73467
Mark Quarasique <julipaha@gmail.com>
2023-10-12 21:16:50 +0300

    fragments logic added

--- app/src/main/java/com/guap/vkr/playlistmaker/library/ui/fragments/FavoriteFragment.kt
+++ app/src/main/java/com/guap/vkr/playlistmaker/library/ui/fragments/FavoriteFragment.kt
@@ -1,20 +1,25 @@
 package com.guap.vkr.playlistmaker.library.ui.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.guap.vkr.playlistmaker.R
+import com.guap.vkr.playlistmaker.databinding.FragmentFavoriteBinding
 
 class FavoriteFragment : Fragment() {
+
+    private lateinit var binding: FragmentFavoriteBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite, container, false)
+    ): View {
+        binding = FragmentFavoriteBinding.inflate(inflater, container, false)
+        binding.ivEmptyList.setImageResource(R.drawable.ic_search_err)
+        binding.tvEmptyList.text = getString(R.string.empty_favorite_text)
+        return binding.root
     }
 
     companion object {
--- app/src/main/java/com/guap/vkr/playlistmaker/library/ui/fragments/PlaylistFragment.kt
+++ app/src/main/java/com/guap/vkr/playlistmaker/library/ui/fragments/PlaylistFragment.kt
@@ -1,20 +1,25 @@
 package com.guap.vkr.playlistmaker.library.ui.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.guap.vkr.playlistmaker.R
+import com.guap.vkr.playlistmaker.databinding.FragmentPlaylistBinding
 
 class PlaylistFragment : Fragment() {
+
+    private lateinit var binding: FragmentPlaylistBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_playlist, container, false)
+        binding = FragmentPlaylistBinding.inflate(inflater, container, false)
+        binding.ivEmptyList.setImageResource(R.drawable.ic_search_err)
+        binding.tvEmptyList.text = getString(R.string.empty_playlists_text)
+        return binding.root
     }
 
     companion object {
