6cd2b8663beab897f9ccab7056583f79a3a22931
Mark Quarasique <julipaha@gmail.com>
2023-12-28 23:15:27 +0300

    Added photopicker on create playlist screen

--- app/src/main/java/com/guap/vkr/playlistmaker/library/ui/fragments/NewPlaylistFragment.kt
+++ app/src/main/java/com/guap/vkr/playlistmaker/library/ui/fragments/NewPlaylistFragment.kt
@@ -1,20 +1,70 @@
 package com.guap.vkr.playlistmaker.library.ui.fragments
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.guap.vkr.playlistmaker.R
 import com.guap.vkr.playlistmaker.databinding.FragmentNewPlaylistBinding
 import com.guap.vkr.playlistmaker.library.ui.view_model.NewPlaylistViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import java.io.File
+import java.io.FileOutputStream
 
 class NewPlaylistFragment : Fragment() {
 
     private var _binding: FragmentNewPlaylistBinding? = null
     private val binding get() = _binding!!
     private val viewModel by viewModel<NewPlaylistViewModel>()
+    private var coverUri: Uri = Uri.EMPTY
+    private val pickMedia =
+        registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+            if (uri != null) {
+                Glide.with(this)
+                    .load(uri.toString())
+                    .placeholder(R.drawable.ic_album_placeholder_2x)
+                    .transform(
+                        CenterCrop(),
+                        RoundedCorners(
+                            requireContext().resources.getDimensionPixelSize(R.dimen._8dp)
+                        )
+                    )
+                    .into(binding.ivPlaylistPicture)
+                coverUri = uri
+                binding.ivPlaylistPicturePlaceholder.visibility = View.GONE
+                saveImageToPrivateStorage(uri)
+            }
+        }
+
+    private fun saveImageToPrivateStorage(uri: Uri) {
+        //создаём экземпляр класса File, который указывает на нужный каталог
+        val filePath = File(requireContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES), "myalbum")
+        //создаем каталог, если он не создан
+        if (!filePath.exists()) {
+            filePath.mkdirs()
+        }
+        //создаём экземпляр класса File, который указывает на файл внутри каталога
+        val file = File(filePath, "first_cover.jpg")
+        // создаём входящий поток байтов из выбранной картинки
+        val inputStream = requireContext().contentResolver.openInputStream(uri)
+        // создаём исходящий поток байтов в созданный выше файл
+        val outputStream = FileOutputStream(file)
+        // записываем картинку с помощью BitmapFactory
+        BitmapFactory
+            .decodeStream(inputStream)
+            .compress(Bitmap.CompressFormat.JPEG, 30, outputStream)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,8 +77,13 @@ class NewPlaylistFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.btnBack.setOnClickListener {
-            findNavController().popBackStack()
+        with(binding) {
+            btnBack.setOnClickListener {
+                findNavController().popBackStack()
+            }
+            ivPlaylistPicture.setOnClickListener {
+                pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+            }
         }
     }
 
